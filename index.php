d16b98d692fb01fea8d66616061368f4562c0212
Chris Haig <chaig200@caledonian.ac.uk>
2019-10-22 09:06:20 +0100

    small changes

--- index.php
+++ index.php
@@ -11,6 +11,22 @@
 
     <div class="container">
     <h1>CROBoard</h1>
+    <div id="nav">
+    <ul>
+        <li><a href="#home">Insert</a></li>
+        <li><a href="#news">Le</a></li>
+        <li><a href="#contact">Categories</a></li>
+        <li style="float:right"><a class="active" href="#about"><?php 
+        
+        if(isset($_SESSION['user'])){
+            echo "Account";
+        }else{
+            echo "Login";
+        }
+
+        ?></a></li>
+    </ul>
+    </div>
     </div>
 
 </body>
