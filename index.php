6d8660ac6619afb97aec5ec4ee004ab361717a4c
Chris Haig <chaig200@caledonian.ac.uk>
2019-10-22 08:42:17 +0100

    inital commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<html>
+
+<head>
+<title>Chris Haig Test Message board</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
+<link rel = "stylesheet" type = "text/css" href ="css/index.css" />
+</head>
+
+<body>
+
+    <div class="container">
+    <h1>CROBoard</h1>
+    </div>
+
+</body>
+
+</html>
